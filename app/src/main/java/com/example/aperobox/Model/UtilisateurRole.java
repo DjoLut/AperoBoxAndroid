6c78acc581cb109da666d87b4d665b6380deb59d
Lutgen <etu32766@henallux.be>
2019-12-16 20:10:09 +0100

    UtilisateurRole

--- app/src/main/java/com/example/aperobox/Model/UtilisateurRole.java
+++ app/src/main/java/com/example/aperobox/Model/UtilisateurRole.java
@@ -4,4 +4,19 @@ public class UtilisateurRole {
     private String idRole;
     private Integer idUtilisateur;
 
+    public String getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(String idRole) {
+        this.idRole = idRole;
+    }
+
+    public Integer getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public void setIdUtilisateur(Integer idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
 }
